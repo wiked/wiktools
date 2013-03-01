dd26bedb91608653e4bfd22ff2837990e9fca23a
William I Krueger <smaerd@gmail.com>
2013-03-01 17:14:21 -0500

    Added beginnings of mouse events

--- wiktools.js
+++ wiktools.js
@@ -1,4 +1,10 @@
 // wiktools.js
+
+// Delacroix is our mouse handler.
+//    "Attention mesdames et messieurs..."
+var delacroix = null
+
+
 function wikWindow(cvs)
 {
   //--------------------------------
@@ -14,6 +20,7 @@ function wikWindow(cvs)
   this.headBorderColor = "#000000";
   this.headColor = "#F0F0F0";
   this.bg = "#FFFFFF";
+  this.otherObjs = [];
 
   //--------------------------------
   //wikWindow.getGraphics()
@@ -60,7 +67,26 @@ function wikWindow(cvs)
     g.fillStyle = this.borderColor;
     g.strokeRect(this.whyx.x, this.whyx.y, this.whyx.w, this.whyx.h);
     this.drawHead();
+
+    if(this.otherObjs && this.otherObjs.length >0)
+    {
+      for(i = 0; i < this.otherObjs.length; ++i)
+      {
+        this.otherObjs[i].draw();
+      }
+    }
   };
+
+  this.addObj = function(obj)
+  {
+    this.otherObjs.push(obj);
+  }
+
+  this.ouiMonsieur = function(evt, pe)
+  {
+    this.otherObjs[0].txt = "(".concat(evt.clientX, ",", evt.clientY,"), (", pe.x, ",", pe.y,")");
+    this.drawWindow();
+  }
 
   //--------------------------------
   //wikWindow.fillCanvasBg()
@@ -74,7 +100,67 @@ function wikWindow(cvs)
     g.fillRect(0,0,this.cvs.width,this.cvs.height);
   };
 
+
+
 };
+
+function wikAddToMouseHandler(obj)
+{
+  if(delacroix)
+  {
+    delacroix.add(obj);
+  }
+}
+
+function wikMouseHandler(cvs)
+{
+  this.cvs = cvs;
+  this.continuer = 1;
+  this.audience = [];
+
+
+  this.personnaliser = function(evt, obj)
+  {
+    return {x:evt.clientX - obj.offsetLeft, y:evt.clientY - obj.offsetTop};
+  }
+
+  this.sourisBouge = function(evt)
+  {
+    if(delacroix.audience)
+    {
+      for(i = 0; i < delacroix.audience.length && delacroix.continuer; ++i)
+      {
+        delacroix.audience[i].ouiMonsieur(evt, delacroix.personnaliser(evt,this));
+      }
+    }
+  }
+
+  this.cvs.onmousemove = this.sourisBouge;
+  this.add = function(obj)
+  {
+    this.audience.push(obj);
+  }
+}
+
+function wikLabel(w)
+{
+  //TODO: type verification!
+  this.prnt = w;
+  this.font = this.font = "10px Georgia";
+  this.txt = "";
+  this.whyx = {x:0,y:0, h:0, w:0};
+  this.fontStyle = "#000000";
+
+  this.draw = function()
+  {
+    var g = this.prnt.getGraphics();
+    g.fillStyle = this.fontStyle;
+    g.font = this.font;
+    var x = this.whyx.x + this.prnt.whyx.x;
+    var y = this.whyx.y + this.prnt.whyx.y;
+    g.fillText(this.txt, x,y);
+  };
+}
 
 
 function wikTestWindow()
@@ -86,7 +172,17 @@ function wikTestWindow()
 
 function wikTest()
 {
+
   var w = wikTestWindow();
   w.whyx = {x: 50, y:100, h: 200, w: 400};
+  delacroix = new wikMouseHandler(w.cvs);
+
+  var t = new wikLabel(w);
+  t.txt = "Text Info";
+  t.whyx = {x: 20, y:190};
+  w.addObj(t);
+  wikAddToMouseHandler(w);
+
+
   w.drawWindow();
 }
